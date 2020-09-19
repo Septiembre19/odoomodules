4be63f00147ae536025428eaddfef6538b945203
Ubuntu <AzureUser@odoo13b.lkyif4zgjgkunahq11vvgxaj1f.bx.internal.cloudapp.net>
2020-09-18 19:43:45 -0500

    [ADD] added panama province on state field

---
+++ tsl_panama_provinces/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "Panama Provincias",
+
+	'summary': """
+		Panama Provincias""",
+
+	'description': """
+		Agrega las provincias y comarcas de Panamá como valores seleccionables dentro 
+		del campo Estado. 
+	""",
+
+	'author': "Luis Dominguez",
+	# 'website': "http://www.yourcompany.com",
+	'category': 'Technical Settings',
+	'maintainer':"Luis Dominguez",
+	'support': 'luis.dominguez19@yahoo.es',
+	'version': '13.0.1.0.0',
+	'license': 'LGPL-3',
+	'depends': ['base'],
+
+	'data': [
+		'data/res_country_state.xml'
+	],
+}
